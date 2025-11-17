4b432343cfc0efe55dae5b0b96797532dbe28733
Richard(Kim,ByoungChul) <skyksit@gmail.com>
2025-11-17 12:36:00 +0900

    disable navigate_to_website

--- src/navigator.rs
+++ src/navigator.rs
@@ -9,8 +9,9 @@ pub struct AndroidNavigatorInterface;
 // TODO: Prompt the user for these things!
 impl NavigatorInterface for AndroidNavigatorInterface {
     fn navigate_to_website(&self, url: Url) {
-        // TODO: Implement `ask` behavior!
-        let _ = webbrowser::open(url.as_ref());
+        // URL 링크 클릭 시 웹브라우저 열기 비활성화
+        log::info!("URL 네비게이션 차단됨: {}", url);
+        // 웹브라우저를 열지 않음
     }
 
     async fn open_file(&self, path: &Path) -> std::io::Result<File> {
